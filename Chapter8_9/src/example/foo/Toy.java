c7e84ba44739ff3ac3134dfef0091dcb7c5a9999
Andrius <andriusjavait@gmail.com>
2017-02-08 21:14:46 +0000

    created Toy bean code, simple old java code

---
+++ Chapter8_9/src/example/foo/Toy.java
@@ -0,0 +1,14 @@
+package example.foo;
+
+public class Toy {
+	// Fields:
+	private String name;
+	// Setters(Mutators):
+    public void setName(String string) {
+        this.name = string;
+    }
+    // Getters(Access):
+    public String getName() {
+        return this.name;
+    }
+}
