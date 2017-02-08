66005da4f2339e1e34922aa25bd27ee3541a1bd8
Andrius <andriusjavait@gmail.com>
2017-02-08 21:27:59 +0000

    update DD with jsp-config and servlet mapping which we created a new skeleton code for servlet MyServlet

---
+++ Chapter8_9/src/example/servlet/MyServlet.java
@@ -0,0 +1,12 @@
+package example.servlet;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import example.foo.*;
+
+public class MyServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request,
+			HttpServletResponse response)throws ServletException,IOException{
+		
+	}
+}
